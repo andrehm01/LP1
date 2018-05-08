f2b4d6023d943bccd668031028e1a991c47a32b0
andrehm01 <38919665+andrehm01@users.noreply.github.com>
2018-05-08 02:00:01 -0300

    Add files via upload

---
+++ ex06.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int soma_quadratica (int x){
+	int soma=0;
+	int i=1;
+	while (i<=x){
+	
+		soma = soma + (i*i);
+		i+=1;
+	}
+	return soma;
+}
+int main () {
+	int n;
+	printf ("Escreva um número: ");
+	scanf ("%d",&n);	
+	n = soma_quadratica(n);
+
+	printf ("valor do somatorio: %d\n",n);
+	return 0;
+}
