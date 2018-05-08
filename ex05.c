ccd78120608ecd76d417cd7a240063147fb9006b
andrehm01 <38919665+andrehm01@users.noreply.github.com>
2018-05-08 01:44:01 -0300

    Add files via upload

---
+++ ex05.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int soma_de_numeros(int x){
+	int soma=x;
+	while (x!=0){
+		printf ("Digite um número: ");
+		scanf ("%d", &x);
+		soma = soma + x;
+		}
+		return soma;
+}
+int main () {
+	int n;
+	printf ("Digite um número: ");
+	scanf ("%d", &n);
+	n = soma_de_numeros(n);
+	
+	printf (" A soma de todos os números e: %d\n", n);
+	return 0;
+}
