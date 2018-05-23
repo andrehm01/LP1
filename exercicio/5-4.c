339ec902f85d6c4569659b75b284b12a87c988e5
andrehm01 <38919665+andrehm01@users.noreply.github.com>
2018-05-23 11:36:53 -0300

    Create 5-4.c

---
+++ exercicio/5-4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void troca(int* p1, int* p2){
+    int aux;
+    aux = *p1;
+    *p1 = *p2;
+    *p2 = aux;
+}
+int main(){
+ int x=10;
+ int y=20;
+ troca(&x,&y);
+ printf("%d %d", x, y);
+
+}
