6afba52fe7d123c390ca45aba32f5863309c29dd
andrehm01 <38919665+andrehm01@users.noreply.github.com>
2018-06-04 15:13:29 -0300

    Add files via upload

---
+++ struct/7-3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+struct Posicao{
+    int x;
+    int y;
+};
+struct Personagem{
+    char imagem;
+    int pont;
+    struct Posicao pos;
+};
+void posicao(struct Posicao *p){
+    printf("informe a posicao no eixo x que o pesonagem se encontra:\n ");
+    scanf ("%d", &((*p).x));
+    printf("informe a posicao no eixo y que o pesonagem se encontra:\n ");
+    scanf ("%d", &((*p).y));
+}
+
+void preenche(struct Personagem *p){
+    printf("informe o caracter para imagem do personagem:\n ");
+    scanf("%c",&((*p).imagem));
+    posicao(& (*p).pos);
+
+
+}
+int main(){
+    int i=0;
+    struct Personagem heroi[i];
+    for (i=0;i<10;i++){
+
+        preenche(&heroi[i]);
+        printf("\nPersonagem: %c - posicao  (%d,%d)\n", heroi[i].imagem, heroi[i].pos.x, heroi[i].pos.y);
+    }
+
+}
