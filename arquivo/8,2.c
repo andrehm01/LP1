421209619ff07270c7b9c7965c03ac2355611c32
andrehm01 <38919665+andrehm01@users.noreply.github.com>
2018-08-08 13:07:51 -0300

    Update 8,2.c

--- arquivo/8,2.c
+++ arquivo/8,2.c
@@ -5,7 +5,7 @@ int main(){
     int i;
     char texto[25];
     FILE*fp;
-    fp=open("arq01.txt","r");
+    fp=fopen("arq01.txt","r");
 
     while(fgets(texto,25,fp)!=NULL){
 		printf("%s",texto);
