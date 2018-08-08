4d5352a9404ea01db3194a0a67fe9d0cd65a8acb
andrehm01 <38919665+andrehm01@users.noreply.github.com>
2018-08-08 13:37:51 -0300

    Update 8-1.c

--- arquivo/8-1.c
+++ arquivo/8-1.c
@@ -3,12 +3,13 @@
 
 int main(void){
     char palavra[25];
-    int i;
+    int i,tam;
     FILE *fp;// cria ponteiro para aquivo
     printf("digite o texto ");
     scanf("%s,palavra");
+    tam=strlen(palavra);
     fp= fopen("arq01.txt","w");
-    for (i=0;i<strlen(palavra);i++);{
+    for (i=0;i<tam;i++);{
         fputc(palavra[i],fp);
     }
     fclose(fp);
