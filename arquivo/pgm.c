b9e010e6c3352911659864efaefb6227088f36a9
andrehm01 <38919665+andrehm01@users.noreply.github.com>
2018-11-07 17:16:19 -0200

    Add files via upload

---
+++ arquivo/pgm.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+	char linha[10],linha2[55];
+	FILE *fp;
+	fp = fopen ("originalPgmP5.pgm","rb");
+	int c,i,j,l,p; //coluna linha 
+	unsigned char str[10];
+	if (fp == NULL){
+		printf("ERRO arquivo não existe");
+	}
+	fscanf(fp,"%s",linha); 
+	printf("%s\n", linha);
+	
+	fgetc(fp);
+	fgets(linha2,55,fp);
+	printf("%s", linha2);
+	fscanf(fp,"%d",&c);
+	printf("%d ", c);
+	fscanf(fp,"%d",&c);
+	printf("%d\n", c);
+	fscanf(fp,"%d",&p);
+	printf("%d\n", p);
+	
+	for (i=0;i<10;i++){
+		fread(str,10,1,fp);
+		printf("\n");
+		for (j=0;j<10;j++){	
+			printf("%u ",str[j]);
+		}
+	}
+	printf("\n");
+	fclose(fp);
+	return 0;
+}
+
+	
+	
